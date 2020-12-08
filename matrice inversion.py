a908ce52fd7ee07e5ed1c87fdb9348d640a5c217
Zhuoyan Wu <73592273+tempo-RUBATO@users.noreply.github.com>
2020-12-08 20:57:36 +0800

    Unpolished matrix-inversion programme
    Yet to be polished programme. Unable to solve literally slightly complex problems.

---
+++ matrice inversion.py
@@ -0,0 +1,34 @@
+import numpy as np
+# N*N simple matrice inversion
+def invert(A:np.array):
+    width=np.size(A,1)
+    height=np.size(A,0)
+    n=height
+    if width!=height:
+        print("non-square!")
+    else:
+        determinant=1/np.linalg.det(A)
+        if(determinant==0):
+            print("uninvertible!")
+        else:
+            E=np.identity(n)
+            AE=np.c_[A,E]
+            for i in range(0,n,1):
+                AE[i]=AE[i]/AE[i,i]
+                for j in range(i+1,n,1):
+                    AE[j]=AE[j]-AE[i]*AE[j,i]
+            for i in range(0,n,1):
+                AE[n-i-1]=AE[n-i-1]/AE[n-i-1,n-i-1]
+                for j in range(i+1,n,1):
+                    AE[n-j-1]=AE[n-j-1]-AE[n-i-1]*AE[n-j-1,n-i-1]
+    return AE
+
+a = np.array([[0.25,0,0,0],
+              [0,0.25,0,0],
+              [0,0,0.2,0],
+              [0,0,0,0.2]],dtype=float)
+print(a)
+a_inv=invert(a)
+print(a_inv)
+
+    
